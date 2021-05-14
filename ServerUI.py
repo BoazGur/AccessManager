606013c946afc3073858d26a4cdfdc2b53183a5b
BoazGur <boazirena@gmail.com>
2021-05-14 14:01:43 +0300

    added moving between pages functionailty

--- ServerUI.py
+++ ServerUI.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 import tkinter.font as tkFont
-from typing import Container
 import pandas as pd
+from functools import partial
 from Server import *
 
 LARGE_FONT = ("Verdana", 20)
@@ -10,6 +10,10 @@ names = pd.read_csv("database/names.csv")
 user = pd.read_csv("database/user.csv")
 
 print(names)
+
+lst_names = [""] + names["name"].tolist()
+
+pages = []
 
 
 class ServerUI(tk.Tk):
@@ -21,23 +25,26 @@ class ServerUI(tk.Tk):
         self.container.grid_rowconfigure(0, weight=1)
         self.container.grid_columnconfigure(0, weight=1)
 
-        self.frames = {}
+        self.frames = []
 
-        for F in (StartPage, PageOne):
-            frame = F(self.container, self)
-            self.frames[F] = frame
+        pages.append(StartPage)
+        for i in range(len(lst_names) - 1):
+            pages.append(PageOne)
+
+        for i, F in enumerate(pages):
+            frame = F(self.container, self, lst_names[i])
+            self.frames.append(frame)
             frame.grid(row=0, column=0, sticky="nsew")
 
-        self.show_frame(StartPage)
+        self.show_frame(0)
 
-    def show_frame(self, cont):
-        # args[o] = name of the user
-        frame = self.frames[cont]
+    def show_frame(self, i):
+        frame = self.frames[i]
         frame.tkraise()
 
 
 class StartPage(tk.Frame):
-    def __init__(self, parent, controller, *args):
+    def __init__(self, parent, controller, name=""):
         tk.Frame.__init__(self, parent)
         # -------------------- Welcome Sign
         frame1 = tk.Frame(self, height=35, bg="red")
@@ -52,38 +59,36 @@ class StartPage(tk.Frame):
         frame2 = tk.Frame(self, width=100, bg="yellow")
         frame2.pack(fill="both", expand=True)
 
-        names = self.get_names()
+        btn_list = []
         j = 0
-        for i, name in enumerate(names):
+        for i, name in enumerate(lst_names[1:]):
             if i % 5 == 0:
                 j += 1
-            btn_name = tk.Button(frame2, text=name, font=LARGE_FONT,
-                                 command=lambda: controller.show_frame(PageOne, name))
-            btn_name.grid(row=j, column=i % 5, padx=10, pady=10)
+            btn_list.append(tk.Button(frame2, text=name, font=LARGE_FONT,
+                                    command=partial(controller.show_frame, i+1)))
+            btn_list[i].grid(row=j, column=i % 5, padx=10, pady=10)
         '''        btn_name1 = tk.Button(frame2, text="name1", font=LARGE_FONT,
-                              command=lambda: controller.show_frame(PageOne))
+                            command=lambda: controller.show_frame(PageOne))
         btn_name1.grid(row=0, column=0, padx=10, pady=10)'''
 
     def get_names(self):
         return names["name"].tolist()
 
+
 class PageOne(tk.Frame):
-    def __init__(self, parent, controller, *args):
+    def __init__(self, parent, controller, name):
         tk.Frame.__init__(self, parent)
+        self.name = name
 
         frame1 = tk.Frame(self, height=35, bg="pink")
         frame1.pack(fill="both")
 
         button1 = tk.Button(frame1, text="Back to Home",
-                            command=lambda: controller.show_frame(StartPage))
+                            command=lambda: controller.show_frame(0))
         button1.pack()
 
-        try:
-            label = tk.Label(frame1, text=f"HELLO {args[0]}!!!!", font=LARGE_FONT)
-            label.pack(pady=10, padx=10)
-        except:
-            label = tk.Label(frame1, text=f"HELLO", font=LARGE_FONT)
-            label.pack(pady=10, padx=10)
+        label = tk.Label(frame1, text=f"HELLO {name}!!!!", font=LARGE_FONT)
+        label.pack(pady=10, padx=10)
 
 
 app = ServerUI()
